b3f63ae4a421fb36bfe2a691bfc0a2b04fc73a01
alex <greathjt@163.com>
2016-12-15 15:47:23 +0800

    target

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -4,13 +4,18 @@ import org.apache.spark.{SparkConf, SparkContext}
   * Created by chao.hu on 2016/12/14.
   */
 object WordCount {
-  def main(args: Array[String]): Unit = {
-    val sc = new SparkContext(new SparkConf().setAppName("Spark Count").setMaster("local[3]"))
-    val threshhold = args(1).toInt
+  def main(args: Array[String]){
+    val sc = new SparkContext(new SparkConf().setAppName("Spark Count"))
+    println(sc.appName)
+    println(sc.getExecutorMemoryStatus)
+    println(sc.getConf)
+    println(sc.master)
+    println(sc.version)
+    val threshhold = 2
     val tokenized = sc.textFile(args(0)).flatMap(_.split(" "))
     val wordCounts = tokenized.map(str => (str, 1)).reduceByKey(_ + _)
     val filtered = wordCounts.filter((_._2 >= threshhold))
     val charCount = filtered.flatMap(_._1.toCharArray).map((_, 1)).reduceByKey(_ + _)
-    System.out.println(charCount.collect().mkString(", "))
+    println(charCount.collect().mkString(", "))
   }
 }
